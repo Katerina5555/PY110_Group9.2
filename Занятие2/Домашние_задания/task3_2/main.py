4f3255258e6dab092e56aba6d3307023bcaf0ab9
Katerina <ErmolinaEI@yandex.ru>
2021-11-26 22:55:42 +0300

    init

--- Занятие2/Домашние_задания/task3_2/main.py
+++ Занятие2/Домашние_задания/task3_2/main.py
@@ -1,3 +1,17 @@
+def output_type_int(fn):
+    def wrapper(*args):
+        result = fn(*args)
+        if not isinstance(result, int):
+            raise TypeError(f"Результатом выполнения функции {fn} должно быть число")
+
+    return wrapper
+
+@output_type_int
+def return_number() -> int:
+
+    return "x"
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    return_number()
+
