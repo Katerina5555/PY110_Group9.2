0df2cc485c7f4bf8aeb2ca4ef2f59c39103b9888
Katerina <ErmolinaEI@yandex.ru>
2021-11-26 22:17:34 +0300

    init

--- Занятие2/Домашние_задания/task1/main.py
+++ Занятие2/Домашние_задания/task1/main.py
@@ -1,5 +1,8 @@
+def task(number: list) -> int:
+    gen_exp = (2 * 2 ** i for i in number)
+    return sum(gen_exp)
 
 
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    pow_ = [i for i in range(1, 11)]
+    print(task(pow_))
--- Занятие2/Домашние_задания/task1_5/main.py
+++ Занятие2/Домашние_задания/task1_5/main.py
@@ -1,7 +1,12 @@
 from string import ascii_lowercase, ascii_uppercase, digits
-
+import random
+def password_():
+    needed_types = ascii_lowercase + ascii_uppercase + digits
+    password = random.sample(needed_types, 8)
+    return "".join(password)
 
 if __name__ == "__main__":
-    print(ascii_lowercase)
-    print(ascii_uppercase)
-    print(digits)
+    # print(ascii_lowercase)
+    # print(ascii_uppercase)
+    # print(digits)
+    print(password_())
