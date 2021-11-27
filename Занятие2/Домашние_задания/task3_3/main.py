ca9f963b840a3b6b0dc3d10fc7c5a762985b3b98
Katerina <ErmolinaEI@yandex.ru>
2021-11-27 12:06:21 +0300

    init

--- Занятие2/Домашние_задания/task3_3/main.py
+++ Занятие2/Домашние_задания/task3_3/main.py
@@ -1,3 +1,24 @@
+def output_type_list(fn):
+    def wrapper(*args, **kwargs):
+
+        fn(*args, **kwargs)
+        for index, arg in enumerate(args):
+            print(f"Позиционный аргумент {index}: {arg}")
+            if arg is not iter:
+                raise TypeError(f"Объект {index} {arg} не является итерируемым")
+
+        for key, kwarg in kwargs.items():
+            print(f"Именованный аргумент {key}: {kwarg}")
+            if kwarg is not iter:
+                raise TypeError(f"Объект {key} {kwarg} не является итерируемым")
+
+    return wrapper
+
+@output_type_list
+def return_list():
+    return [343]
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    return_list()
+
